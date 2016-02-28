bbbc7d9a95862401a5d11fc37c26f2d2a012dbea
srlin <ntp890517.ee96@g2.nctu.edu.tw>
2016-02-27 19:31:59 -0700

    pure virtual Solver::Preprocess()

--- src/solver/Solver.h
+++ src/solver/Solver.h
@@ -12,8 +12,11 @@ class Solver {
             UNSAT,
             SAT,
         };
+
         Solver():_result(UNDEF), _debug(false) {};
+
         virtual void ReadCnf(const string cnfName) = 0;
+        virtual void Preprocess() = 0;
         virtual void Solve() = 0;
         virtual string GetAssignment() = 0;
 
