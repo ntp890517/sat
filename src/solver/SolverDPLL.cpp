dd7f35a3619da6c42ab9f4d93f4d9fe8ffdba8c9
srlin <ntp890517.ee96@g2.nctu.edu.tw>
2016-03-25 05:44:12 -0700

    develope SolverDPLL

--- src/solver/SolverDPLL.cpp
+++ src/solver/SolverDPLL.cpp
@@ -1,5 +1,64 @@
 #include "SolverDPLL.h"
 
+void SolverDPLL::ReadCnf(const string cnfName) {
+    ifstream fin;
+    fin.open(cnfName.c_str());
+    unsigned int nVar, nCl;
+
+    string line;
+    string buf;
+    while(getline(fin, line)) {
+        istringstream ss(line);
+        if (ss.peek() == 'c') {
+            continue;
+        } else if (ss.peek() == 'p') {
+            ss >> buf >> buf >> nVar >> nCl;
+            InitVariables(nVar);
+        } else if (ss.peek() == '0') {
+            break;
+        } else {
+            _clauses.push_back(ParseClause(ss.str()));
+        }
+    }
+
+    fin.close();
+
+    MSG() << cnfName << " is parsed. "
+          << " nVar: " << GetNumOfVariables()
+          << " nClause: " << GetNumOfClauses() << endl;
+    if (IsDebug()) {
+        Display();
+    }
+}
+
+Clause* SolverDPLL::ParseClause(string s) {
+    int lit;
+    istringstream iss(s);
+    Clause *c = new Clause();
+    Literal *pLit = NULL;
+
+    while(iss >> lit) {
+        if (lit == 0) {
+            break;
+        } else if (lit > 0) {
+            pLit = _variables[lit]->GetPosLit();
+            c->Insert(pLit);
+        } else {
+            pLit = _variables[-lit]->GetNegLit();
+            c->Insert(pLit);
+        }
+    }
+    return c;
+}
+
+string SolverDPLL::GetAssignment() { stringstream ss;
+    for (unsigned int i = 1 ; i < GetNumOfVariables() + 1 ; i++) {
+        ss << (_variables[i]->GetAssignedValue()? "":"-") 
+           << i << ' ';
+    }
+    return ss.str();
+}
+
 void SolverDPLL::Solve()
 {
     _result = Preprocess();
@@ -25,6 +84,18 @@ void SolverDPLL::Solve()
                 break;
             }
         }
+    }
+}
+
+void SolverDPLL::InitVariables(const unsigned int n) {
+    for (unsigned i = 0 ; i < n+1 ; i++) {
+        _variables.push_back(new Variable(i));
+    }
+}
+
+void SolverDPLL::Display() {
+    for (unsigned i = 0 ; i < _clauses.size() ; i++) {
+        cout << _clauses[i]->GetString() << endl;
     }
 }
 
--- src/solver/SolverDPLL.h
+++ src/solver/SolverDPLL.h
@@ -2,17 +2,32 @@
 #define SOLVERDPLL_H_
 
 #include "Solver.h"
+#include <iostream>
+#include <fstream>
+#include "../ut/ut.h"
 
 typedef unsigned int Level;
 
 class SolverDPLL : public Solver {
     public:
+        virtual void ReadCnf(const string cnfName);
         virtual void Solve();
+        virtual string GetAssignment();
+
+        unsigned int GetNumOfClauses() {return _clauses.size();}
+        unsigned int GetNumOfVariables() {return _variables.size() - 1;}
+
+        void Display();
     private:
+        void InitVariables(unsigned int);
+        Clause* ParseClause(string s);
         Solver::Result Preprocess();
         void Decide();
         Solver::Result Deduce();
         Level Analyze();
         void BackTrack(Level lv);
+    private:
+        vector<Clause*> _clauses;
+        vector<Variable*> _variables;
 };
 #endif
