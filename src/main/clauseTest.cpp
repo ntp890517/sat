bc0bdc9d7e9ce06f2bd7746faf23a7bbfa56f10d
srlin <ntp890517.ee96@g2.nctu.edu.tw>
2016-04-06 02:59:03 -0700

    test for Clause2Watch

--- src/main/clauseTest.cpp
+++ src/main/clauseTest.cpp
@@ -1,4 +1,5 @@
 #include "../db/Clause.h"
+#include "../db/Clause2Watch.h"
 #include <iostream>
 
 using namespace std;
@@ -29,6 +30,28 @@ int main() {
 
     cout << "[TEST] Get(0)" << endl;
     cout << c->Get(0)->GetNumeric() << endl;
+
+    Clause2Watch* c2w = new Clause2Watch();
+    c2w->Insert(var1->GetPosLit());
+    c2w->Insert(var2->GetNegLit());
+    c2w->Insert(var3->GetPosLit());
+
+    cout << "Clause2Watch = (1 -2 3)" << endl;
+    c2w->Setup2Watch();
+
+    cout << "[TEST] Original (w1, w2)" << endl;
+    cout << c2w->GetWatch1()->GetString() << ", " << c2w->GetWatch2()->GetString() << endl;
+
+    cout << "[TEST] 1=F (w1, w2)" << endl;
+    var1->Assign(false);
+    c2w->Update2Watch(var1->GetPosLit());
+    cout << c2w->GetWatch1()->GetString() << ", " << c2w->GetWatch2()->GetString() << endl;
+
+    cout << "[TEST] 2=T (w1, w2)" << endl;
+    var2->Assign(true);
+    Literal * impLit = c2w->Update2Watch(var2->GetNegLit());
+    cout << c2w->GetWatch1()->GetString() << ", " << c2w->GetWatch2()->GetString() << endl;
+    cout << "imply: " << impLit->GetString() << endl;
 
     return 0;
 }
